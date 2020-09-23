3aeddf0fefb378d6d7f354bfcb7616ba5055ef97
Gusbod <skrapiskrap@gmail.com>
2020-09-23 22:47:11 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H5Chocolate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello H5Chocolate!");
+        }
+    }
+}
